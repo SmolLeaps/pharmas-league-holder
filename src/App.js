824c24bac25d172b3e4c44336925b4071e7c7ea2
Callista Chang <changcallista@gmail.com>
2021-05-10 21:07:56 +0800

    Small changes

--- src/App.js
+++ src/App.js
@@ -1,74 +1,95 @@
-import React, {useEffect, useState} from 'react'
-import Routes from './Routes'
-import { Link, withRouter } from 'react-router-dom'
-import { LinkContainer } from 'react-router-bootstrap'
-import { Nav, Navbar, NavItem } from 'react-bootstrap'
-import './App.css'
-import LOGO from './static/images/logo.png'
+import React, { useEffect, useState } from "react";
+import Routes from "./Routes";
+import { Link, withRouter } from "react-router-dom";
+import { LinkContainer } from "react-router-bootstrap";
+import { Nav, Navbar, NavItem } from "react-bootstrap";
+import "./App.css";
+import LOGO from "./static/images/company-name.png";
 
-import SdkService from './utils/sdkService'
-import {MessageService} from "./utils/messageService";
+import SdkService from "./utils/sdkService";
+import { MessageService } from "./utils/messageService";
 
 function App(props) {
-  window.sdk = new SdkService()
-  const [isAuthenticated, userHasAuthenticated] = useState(false)
+  window.sdk = new SdkService();
+  const [isAuthenticated, userHasAuthenticated] = useState(false);
   const [shareRequestToken, setShareRequestToken] = useState(null);
   const [acceptVCLink, setAcceptVCLink] = useState(null);
 
   useEffect(() => {
     if (isAuthenticated) {
-      window.sdk.init().then(networkMember => {
-        window.messageService = new MessageService(networkMember)
-      }).catch(console.error)
+      window.sdk
+        .init()
+        .then((networkMember) => {
+          window.messageService = new MessageService(networkMember);
+        })
+        .catch(console.error);
     } else {
-      window.messageService = null
+      window.messageService = null;
     }
-  }, [isAuthenticated])
+  }, [isAuthenticated]);
 
   async function handleLogout(event) {
-    event.preventDefault()
-    userHasAuthenticated(false)
+    event.preventDefault();
+    userHasAuthenticated(false);
 
     try {
-      await window.sdk.signOut()
-      props.history.push('/login')
+      await window.sdk.signOut();
+      props.history.push("/login");
     } catch (error) {
-      console.log('error on logout', error)
+      console.log("error on logout", error);
     }
   }
 
   return (
-    <div className='App'>
-        <Navbar>
-          <Navbar.Header>
-            <Navbar.Brand>
-              <Link to='/'>
-                <img src={LOGO} alt="logo" style={{height: '50px', paddingLeft: '20px', marginBottom: '20px'}} /> 
-              </Link>
-            </Navbar.Brand>
-            <Navbar.Toggle />
-          </Navbar.Header>
-          <Navbar.Collapse>
-            <Nav pullRight>
-              { isAuthenticated
-                ? <>
-                    <NavItem onClick={handleLogout}>Logout</NavItem>
-                  </>
-                : <>
-                    <LinkContainer to='/signup'>
-                      <NavItem>Sign Up</NavItem>
-                    </LinkContainer>
-                    <LinkContainer to='/login'>
-                      <NavItem>Login</NavItem>
-                    </LinkContainer>
-                  </>
-              }
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-        <Routes appProps={{ isAuthenticated, userHasAuthenticated, shareRequestToken,  setShareRequestToken, acceptVCLink, setAcceptVCLink}} />
+    <div className="App">
+      <Navbar>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <Link to="/">
+              <img
+                src={LOGO}
+                alt="logo"
+                style={{
+                  height: "50px",
+                  // paddingLeft: "20px",
+                  // marginBottom: "20px",
+                }}
+              />
+            </Link>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav pullRight>
+            {isAuthenticated ? (
+              <>
+                <NavItem onClick={handleLogout}>Logout</NavItem>
+              </>
+            ) : (
+              <>
+                <LinkContainer to="/signup">
+                  <NavItem>Sign Up</NavItem>
+                </LinkContainer>
+                <LinkContainer to="/login">
+                  <NavItem>Login</NavItem>
+                </LinkContainer>
+              </>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <Routes
+        appProps={{
+          isAuthenticated,
+          userHasAuthenticated,
+          shareRequestToken,
+          setShareRequestToken,
+          acceptVCLink,
+          setAcceptVCLink,
+        }}
+      />
     </div>
-  )
+  );
 }
 
-export default withRouter(App)
+export default withRouter(App);
